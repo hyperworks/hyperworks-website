32ed7807cb6f0cbb38fb2235ba5a8dcdf73609df
Matthew Kanwisher <matt@kanwisher.com>
2015-05-20 11:26:25 +0700

    fixing build

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,7 @@ var awspublish = require('gulp-awspublish');
 gulp.task('publish', function() {
  
   // create a new publisher 
-  var publisher = awspublish.create({ bucket: 'www.hyperworks.co.th' });
+  var publisher = awspublish.create({params: { Bucket: 'www.hyperworks.co.th' }});
  
   // define custom headers 
   var headers = {
